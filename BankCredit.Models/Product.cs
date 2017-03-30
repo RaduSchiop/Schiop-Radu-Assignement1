51b24f1fc471c89fb75b1227dd47c64e1f19fdfe
RaduSchiop <radux90@yahoo.com>
2017-03-30 18:43:08 +0300

    Add files via upload

---
+++ BankCredit.Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankCredit.Models
+{
+   public class Product
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string color { get; set; }
+        public string size { get; set; }
+        public int price { get; set; }
+        public int stok { get; set; }
+    }
+}
